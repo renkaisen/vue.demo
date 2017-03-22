189fb66fb84e3ac9cd70986324a60ff56f868389
kaixin <276573809@qq.com>
2017-03-22 12:01:20 +0800

    关于webpack的配置

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <h3>牵着一头小鹿走</h3>
+    <strong>{{ msg }}</strong>
+    <input type="button" value="您快来点我，你好牛逼啊" @click = 'change'>
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+
+export default {
+  data () {
+    return {
+      msg: '真的很niub'
+    }
+  },
+  components: {
+    Hello
+  }
+}
+
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+strong{
+  background: red;
+}
+
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>会是小鹿</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '我学习中的，vue 这个规范好难受!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
